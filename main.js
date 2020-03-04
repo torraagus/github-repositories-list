e79a136df5f0ba2c948dfca12314c56b6b5e5b92
Torra Agustin <torra.agustinignacio@gmail.com>
2020-03-04 11:50:46 -0300

    implementation of bootstrap in repos list

--- main.js
+++ main.js
@@ -10,14 +10,24 @@ function getRepositories() {
     .then(data => {
         let ul = document.getElementById('repoList');
         data.forEach(repo => {
-            var li = document.createElement("li");
-            var avatar = document.createElement("img");
-            var repo_name = document.createElement("p");
-            avatar.setAttribute('src', `${repo.owner.avatar_url}`);
-            repo_name.innerHTML = repo.full_name;
-            li.appendChild(avatar);
-            li.appendChild(repo_name);
-            ul.appendChild(li);
+            var row = document.createElement("div");
+            for (let i = 0; i < 4; i++) {
+                var col = document.createElement("div");
+                var avatar = document.createElement("img");
+                var repo_full_name = document.createElement("p");
+                var repo_owner_login = document.createElement("p");
+                avatar.setAttribute('src', `${repo.owner.avatar_url}`);
+                repo_full_name.innerHTML = repo.full_name;
+                repo_owner_login.innerHTML = `from ${repo.owner.login}`;
+                row.classList.add("row");
+                col.classList.add("col-3");
+                col.classList.add("repository");
+                col.appendChild(avatar);
+                col.appendChild(repo_full_name);
+                col.appendChild(repo_owner_login);
+                row.appendChild(col);                    
+            }            
+            ul.appendChild(row);
         });
         console.log(data);
     }) 
